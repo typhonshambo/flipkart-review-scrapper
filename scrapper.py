132be68d1f6b57ed2ea2c61163b0d6db679a8c99
Shreya Bhalgat <85868386+shreyabhalgat@users.noreply.github.com>
2023-10-25 12:41:19 +0530

    Update scrapper.py
    Added error handling functionality to handle non-200 status codes during web scraping.

--- scrapper.py
+++ scrapper.py
@@ -2,55 +2,63 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def error_handler(response):
+    if response.status_code != 200:
+        print(f"Error: Unable to fetch data. Status code {response.status_code}")
+        return True
+    return False
 
 def scraper(keyword):
     url = "https://www.flipkart.com/search?q="
-    r = requests.get(url+keyword)
+    r = requests.get(url + keyword)
+    
+    if error_handler(r):
+        return
+    
     pageSource = r.text
     soup = BeautifulSoup(pageSource, "html.parser")
-    box = soup.findAll("div", {"class" : "_13oc-S"})
-    title = (soup.findAll("div", {"class" : "CXW8mj"}))
+    box = soup.findAll("div", {"class": "_13oc-S"})
+    title = (soup.findAll("div", {"class": "CXW8mj"}))
     links = []
     headings = []
-    
-    #product titles
+
+    # product titles
     for i in title:
         val = i.img['alt']
         headings.append(val)
-    
-  
 
-
-    #getting product links
+    # getting product links
     for i in box:
-
         val = i.div.div.a['href']
         links.append(val)
 
-
-    #print(links)
     datas = []
-    #getting reviews
+
+    # getting reviews
     print("loading...")
     for j in range(0, len(links)):
-        r = requests.get("https://www.flipkart.com" +links[j])
+        r = requests.get("https://www.flipkart.com" + links[j])
+        if error_handler(r):
+            continue
+        
         pageSource = r.text
         soup = BeautifulSoup(pageSource, "html.parser")
-        box = soup.findAll("div", {"class" : "t-ZTKy"})
+        box = soup.findAll("div", {"class": "t-ZTKy"})
 
         reviews = []
         for i in box:
             val = (i.div.div).text
             reviews.append(val)
-        
+
         data = {
-            f"{headings[j]}" : reviews
+            f"{headings[j]}": reviews
         }
         datas.append(data)
 
-    
     with open("data.json", "w") as f:
         json.dump(datas, f)
 
     print("Done check data.json")
 
+# Call the function
+scraper("laptop")
