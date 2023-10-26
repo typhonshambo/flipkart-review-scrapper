2e36ab1602fff1fdf3fbe36884bbdba3d796eaa6
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2023-10-26 18:41:21 +0530

    added website

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from flask import Flask, render_template, request
 from scrapper import scraper
+import json
 
-keyword = str(input("What you want to search : "))
-scraper(keyword)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=None)
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+    scraper(keyword)  # Perform scraping for the provided keyword
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -5,52 +5,48 @@ import json
 
 def scraper(keyword):
     url = "https://www.flipkart.com/search?q="
-    r = requests.get(url+keyword)
+    r = requests.get(url + keyword)
     pageSource = r.text
     soup = BeautifulSoup(pageSource, "html.parser")
-    box = soup.findAll("div", {"class" : "_13oc-S"})
-    title = (soup.findAll("div", {"class" : "CXW8mj"}))
+    box = soup.findAll("div", {"class": "_13oc-S"})
+    title = soup.findAll("div", {"class": "CXW8mj"})
     links = []
     headings = []
-    
-    #product titles
+
+    # product titles
     for i in title:
         val = i.img['alt']
         headings.append(val)
-    
-  
 
+    datas = []  # Initialize an empty list for storing product entries
 
-    #getting product links
+    # getting product links
     for i in box:
-
         val = i.div.div.a['href']
         links.append(val)
 
-
-    #print(links)
-    datas = []
-    #getting reviews
+    # getting reviews
     print("loading...")
     for j in range(0, len(links)):
-        r = requests.get("https://www.flipkart.com" +links[j])
+        r = requests.get("https://www.flipkart.com" + links[j])
         pageSource = r.text
         soup = BeautifulSoup(pageSource, "html.parser")
-        box = soup.findAll("div", {"class" : "t-ZTKy"})
+        box = soup.findAll("div", {"class": "t-ZTKy"})
 
         reviews = []
         for i in box:
             val = (i.div.div).text
             reviews.append(val)
-        
-        data = {
-            f"{headings[j]}" : reviews
+
+        # Create a product entry as a dictionary
+        product_entry = {
+            "product_name": headings[j],
+            "reviews": reviews
         }
-        datas.append(data)
+        datas.append(product_entry)  # Append the product entry to the list
 
-    
     with open("data.json", "w") as f:
-        json.dump(datas, f)
+        json.dump(datas, f, indent=4)  # Dump the list of product entries to the JSON file
 
-    print("Done check data.json")
+    print("Done, check data.json")
 
